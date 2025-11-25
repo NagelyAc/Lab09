919652670189e66e2fc9386616d9493a34de37ec
NagelyAc <nagely.arpi@tecsup.edu.pe>
2025-11-25 18:56:35 -0500

    Agregacion de permisos de Internet y las dependencias Creacion de modelo PostModel, Interfaz API (PostApiService) y Configuración de Retrofit

---
+++ app/src/main/java/com/example/lab09/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.lab09
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.lab09.ui.theme.Lab09Theme
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab09Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ProgPrincipal9(){
+    val urlBase="https://json-placeholder.mock.beeceptor.com/"
+    val retrofit = Retrofit.Builder().baseUrl(urlBase)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {BarraSuperior()},
+        bottomBar = {BarraInferior(navController) },
+        content = { paddingValues -> Contenido(paddingValues, navController, servicio)}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab09Theme {
+        Greeting("Android")
+    }
+
+
+
+}
